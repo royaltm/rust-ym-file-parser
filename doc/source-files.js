9650ef081add8e4cd789f8283fbfed9cb788736a
Rafal Michalski <royaltm75@gmail.com>
2020-07-22 15:55:38 +0200

    update docs

---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ym_file_parser"] = {"name":"","dirs":[{"name":"ym","files":["effects.rs","flags.rs","parse.rs","player.rs"]}],"files":["lib.rs","ym.rs"]};
+createSourceSidebar();
---
+++ doc/ym_file_parser/flags/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["FxChannel",""],["FxType",""]],"struct":[["FxCtrlFlags",""],["SongAttributes",""]]});
